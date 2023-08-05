6faf454007437de0b500a31f2e49011bd3798e69
NMoreWork <141286601+NMoreWork@users.noreply.github.com>
2023-08-05 06:31:09 +0300

    Update pyton3.py

--- pyton3.py
+++ pyton3.py
@@ -1,1 +1,18 @@
  dfsdfsdf
+sdfsasflmn 
+as fak a[a
+ a 
+ga ag 
+sd a
+dd ds
+ ds sd
+ sg
+gsdg sdgdsffgsfdg sdg fd s sd
+s sd g
+s 
+sd gds
+g dsddsgds
+ sdfgfsg sdgajh  k uha   
+
+   jzh f jj u ,muhejllidsdu 'zf a
+s jla  
